82465ac6a0862f7851d6920140370ed25b9bdd1a
brianoy <brian20020925@gmail.com>
2022-12-21 03:27:39 +0800

    Add files via upload

---
+++ discord_notify.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+dcUrl = "<discord webhook url>"
+
+magnitude = str(sys.argv[1]).replace("+","強").replace("-","弱")
+second = int(sys.argv[2])
+
+def dcWebhook(payload)->None:
+    data = {"content": str(payload)}
+    requests.post(dcUrl, json=data)
+
+msg = "警告：地區預計震度" + magnitude + "級地震\n預計到達時間:" + second + "秒"
+
+dcWebhook(msg)
+
+
---
+++ earthquick.bat
@@ -0,0 +1,6 @@
+echo %1 %2
+rem 傳入%1 %2兩個引數
+python C:\Users\brian\Desktop\earthquick\line_notify.py %1 %2
+rem 叫pyhton執行在這個路徑的檔案並傳入%1 %2
+Pause
+rem 讓視窗停留
